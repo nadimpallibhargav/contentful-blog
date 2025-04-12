3a2ca17edb2c81cba0eaf67cb8ce79fb6850f2d1
nadimpalli bhargav <bhargavraju98@gmail.com>
2025-04-12 16:20:49 +0000

    💄 removed translate

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,24 +1,17 @@
-import { headers } from 'next/headers';
 import Link from 'next/link';
-import { Trans } from 'react-i18next/TransWithoutContext';
-
 import { Container } from '@src/components/shared/container';
-import initTranslations from '@src/i18n';
-import { defaultLocale } from '@src/i18n/config';
 
-export default async function NotFound() {
-  const headersList = headers();
-  const locale = headersList.get('x-next-i18n-router-locale') || defaultLocale;
-  const { t } = await initTranslations({ locale });
-
+export default function NotFound() {
   return (
     <Container>
-      <title>{t('notFound.title')}</title>
-      <h1 className="h2">{t('notFound.title')}</h1>
+      <title>Page Not Found</title>
+      <h1 className="h2">Page Not Found</h1>
       <p className="mt-4">
-        <Trans i18nKey="notFound.description" t={t}>
-          <Link className="text-blue500" href="/" />
-        </Trans>
+        Sorry, we couldn’t find the page you’re looking for.{' '}
+        <Link className="text-blue500" href="/">
+          Go back home
+        </Link>
+        .
       </p>
     </Container>
   );
